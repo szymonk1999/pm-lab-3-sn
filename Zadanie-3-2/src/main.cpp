5ca3ac46c44b7688d5e7344bc9d3a02f68fb687d
„Szymon <szymonk@onet.pl>
2020-12-14 09:24:43 +0100

    Wykonano  zadanie 3.2.1

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
-
+uint8_t i = 0;
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Wiraj programisto!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println(i);
+  delay(2000);
 }
