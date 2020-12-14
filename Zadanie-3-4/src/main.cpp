bf517c23f253163fb7405e6548f267e7177a99b4
„Szymon <szymonk@onet.pl>
2020-12-14 17:08:52 +0100

    Wykonano zadanie 3.4.2

--- Zadanie-3-4/src/main.cpp
+++ Zadanie-3-4/src/main.cpp
@@ -1,20 +1,13 @@
-#include <Arduino.h>
-
-#define PIN_G 5
-#define PIN_B 6
-#define PIN_R 9
-
-int r,g,b;
-
-void setup() {
-  pinMode(PIN_G, OUTPUT);
-  pinMode(PIN_B, OUTPUT);
-  pinMode(PIN_R, OUTPUT);
-}
-
-void loop() {
-  analogWrite(PIN_R, rand());
-  analogWrite(PIN_G, rand());
-  analogWrite(PIN_B, rand());
-  delay(1000);
-}
+#include <avr/io.h>
+ int main()
+ {
+   DDRD&=!(1<<PD1);
+   DDRD|=(1<<PD2);
+   while(1)
+   {
+     if(!(PIND&(1<<PD1)))
+       PORTD&=!(1<<PD2);
+     else
+       PORTD|=(1<<PD2);
+   }
+ }
